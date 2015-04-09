e3c3d2c13ff0d9b1d123dd7bfb7d798a269d6fce
aureliejollet <aurelie.jollet@gmail.com>
2015-04-09 18:06:30 +0200

    Tools package : add distanceEuclideanRound

--- src/main/java/org/vrprep/solchecker/tools/DistanceCalculator.java
+++ src/main/java/org/vrprep/solchecker/tools/DistanceCalculator.java
@@ -1,5 +1,6 @@
 package org.vrprep.solchecker.tools;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
 
@@ -28,6 +29,31 @@ public class DistanceCalculator {
     }
     
     /**
+     * Calculates the euclidean distance between two points a and b.
+     * @param a Cordinates of the first point {x, y, ...}
+     * @param b Cordinates of the second point {x, y, ...}
+     * @param nbDecimal
+     * @param roundingMode
+     * @return Euclidean distance between a and b.
+     */
+    public static double euclideanDistanceRound(Double [] a, Double [] b, int nbDecimal, int roundingMode){
+        int dimension = Math.min(a.length, b.length);
+        double distance = 0;
+        
+        for(int i = 0; i < dimension; ++i){
+            distance += Math.pow(a[i] - b[i], 2);
+        } 
+        
+        distance = Math.sqrt(distance);
+        
+        BigDecimal bigDecimal = new BigDecimal(distance);
+        bigDecimal = bigDecimal.setScale(nbDecimal, roundingMode);
+        distance = bigDecimal.doubleValue();
+        
+        return distance;
+    }
+    
+    /**
      * Calculates the euclidean distance traveled by the route. 
      * @param route List of customers visited during the route
      * @param cordinates Map containing for each customer its cordinates
@@ -40,6 +66,31 @@ public class DistanceCalculator {
             int a = route.get(i);
             int b = route.get(i+1);
             distance += euclideanDistance(cordinates.get(a), cordinates.get(b));
+        }
+        
+        return distance;
+    }
+    
+    /**
+     * Calculates the euclidean distance traveled by the route. 
+     * @param route List of customers visited during the route
+     * @param cordinates Map containing for each customer its cordinates
+     * @param nbDecimal
+     * @param roundingMode
+     * @return Euclidean distance traveled by the route
+     */
+    public static double euclideanDistanceRoundOnRoute(
+            List<Integer> route, 
+            Map<Integer, Double[]> cordinates, 
+            int nbDecimal, 
+            int roundingMode){
+        
+        double distance = 0;
+        
+        for(int i = 0; i < (route.size() - 1); ++i){
+            int a = route.get(i);
+            int b = route.get(i+1);
+            distance += euclideanDistanceRound(cordinates.get(a), cordinates.get(b), nbDecimal, roundingMode);
         }
         
         return distance;
